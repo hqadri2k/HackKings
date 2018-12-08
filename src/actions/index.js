c3cc51f3a8e3c08a7f70d05b1ceed42d74872c67
Femi Oladipo <oluwafemi.oladipo@kcl.ac.uk>
2018-12-08 14:29:44 +0000

    bootstrap react

--- src/actions/index.js
+++
@@ -1,3 +0,0 @@
-export function selectBook(book) {
-  console.log(book);
-}
--- src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default () => {
-  return [
-    {title: 'Javascript: The Good Parts'},
-    {title: 'Harry Potter and the goblet of fire'},
-    {title: 'The dark tower'},
-    {title: 'Eloquent Ruby'},
-  ]
-}
