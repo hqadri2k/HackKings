a316aaeab5b22644eb49cc3f031e3a20b0112af7
Femi Oladipo <oluwafemi.oladipo@kcl.ac.uk>
2018-12-08 14:17:33 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
+import { createStore } from 'redux';
+import { Provider } from "react-redux";
+
+let store = createStore(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>, 
+  document.getElementById('root')
+);
+registerServiceWorker();
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default () => {
+  return [
+    {title: 'Javascript: The Good Parts'},
+    {title: 'Harry Potter and the goblet of fire'},
+    {title: 'The dark tower'},
+    {title: 'Eloquent Ruby'},
+  ]
+}
